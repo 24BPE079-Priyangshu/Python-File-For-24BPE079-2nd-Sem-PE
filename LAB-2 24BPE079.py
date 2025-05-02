3fc8decdc36ef0600a2401cae33c8ec7afbfad13
24BPE079-Priyangshu <24bpe079@spt.pdpu.ac.in>
2025-05-02 10:09:42 +0530

    Create LAB-2 24BPE079.py

---
+++ LAB-2 24BPE079.py
@@ -0,0 +1,8 @@
+Priyangshu Kumar Das - 24BPE079
+Q1:
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print(f"Largest: {a}, Smallest: {b}")
+else:
+    print(f"Largest: {b}, Smallest: {a}")
